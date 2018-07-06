0b40ff51eca89854a12b1ae2b0fcc89fdabb5c2a
Buhanov-Dima <35217770+Buhanov-Dima@users.noreply.github.com>
2018-07-06 21:58:26 +0500

    адаптив 993+

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	<title>DENEUM</title>
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=Edge"/>
-    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 	<link rel="stylesheet" href="/css/owl-carousel/owl.carousel.min.css">
 	<link rel="stylesheet" href="/css/owl-carousel/owl.theme.default.min.css">
@@ -12,6 +12,7 @@
 	<link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700,800,900" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Roboto+Mono:300,400,500,700" rel="stylesheet">
 	<link rel="stylesheet" href="/css/style.css">
+	<link rel="stylesheet" href="css/custom.css" media="only screen and (max-width:1550px)">
 	<link rel="stylesheet" href="css/style-lg.css" media="only screen and (max-width:1200px)">
 	<link rel="stylesheet" href="css/style-xl.css" media="only screen and (max-width:992px)">
 	<link rel="stylesheet" href="css/style-md.css" media="only screen and (max-width:768px)">
@@ -132,11 +133,13 @@
 		<div class="container-fluid pdg-0">
 			<div class="row">
 				<div class="col">
-					<div class="slaider owl-carousel">
-					  <div><img src="/img/slaider/forbes.svg" alt=""></div>
-					  <div><img src="/img/slaider/china.svg" alt=""></div>
-					  <div><img src="/img/slaider/nasdaq.svg" alt=""></div>
-					  <div><img src="/img/slaider/coin.svg" alt=""></div>
+					<div class="wrap-slaider">
+						<div class="slaider owl-carousel">
+						  <div><img src="/img/slaider/forbes.svg" alt=""></div>
+						  <div><img src="/img/slaider/china.svg" alt=""></div>
+						  <div><img src="/img/slaider/nasdaq.svg" alt=""></div>
+						  <div><img src="/img/slaider/coin.svg" alt=""></div>
+						</div>
 					</div>
 				</div>
 			</div>
@@ -530,13 +533,49 @@
 					<div class="wrap-b11">
 						<div class="team_item">
 							<div class="img-circle">
-								<img src="/img/team/" alt="">
+								<img src="/img/team/jeff.svg" alt="">
 							</div>
-							<div>
+							<div class="team_txt">
 								<h2>Jeff McMahon. Forbes</h2>
 								<div class="square">
 									<i></i><i></i>
 									<p>“…It really is the beginning of an enormous change in how the electric system is organized, and it’s a problem for the utilities that don’t get on board”</p>
+								</div>
+							</div>
+						</div>
+						<div class="team_item">
+							<div class="img-circle">
+								<img src="/img/team/musk.svg" alt="">
+							</div>
+							<div class="team_txt">
+								<h2>Elon Musk. Tesla. SpaceX</h2>
+								<div class="square">
+									<i></i><i></i>
+									<p>“…I think that sustainable energy is obviously really important. If it’s not sustainable it is unsustainable”</p>
+								</div>
+							</div>
+						</div>
+						<div class="team_item">
+							<div class="img-circle">
+								<img src="/img/team/krapels.svg" alt="">
+							</div>
+							<div class="team_txt">
+								<h2>Ed Krapels. Anbaric</h2>
+								<div class="square">
+									<i></i><i></i>
+									<p>“..we’re now seeing a revolutionary change in the organization of consumers by marrying up distributed energy generation with blockchain”</p>
+								</div>
+							</div>
+						</div>
+						<div class="team_item">
+							<div class="img-circle">
+								<img src="/img/team/buterin.svg" alt="">
+							</div>
+							<div class="team_txt">
+								<h2>Vitalik Buterin. Etherium</h2>
+								<div class="square">
+									<i></i><i></i>
+									<p>“..I would personally feel very unhappy if my main contribution to the world was adding Cyprus’s worth of electricity consumption to global warming”</p>
 								</div>
 							</div>
 						</div>
@@ -546,7 +585,191 @@
 		</div>
 	</section>
 
-	<footer></footer>
+	<section id="b12">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-xl-12">
+					<h1 class="standart-h1">TEAM AND ADVISORS</h1>
+					<div class="wrap-b12">
+						<div class="team_item">
+							<div class="img-circle">
+								<img src="/img/team/bogdan.svg" alt="">
+							</div>
+							<div class="team_txt">
+								<div class="b12-team-txt"><h4>Bogdan Fiedur</h4><h4>Advisor</h4></div>
+								<div class="square">
+									<i></i><i></i>
+									<p>Bogdan Fiedur is a smart contract developer, crypto-investor, entrepreneur and President of blockchainexperts.io .  He has over 20 years of IT experience in building e-commerce websites and has been involved in blockchain development during last 3 years.</p>
+								</div>
+							</div>
+						</div>
+						<div class="team_item">
+							<div class="img-circle">
+								<img src="/img/team/wulf.svg" alt="">
+							</div>
+							<div class="team_txt">
+								<div class="b12-team-txt"><h4>Wulf Kaal</h4><h4>Advisor</h4></div>
+								<div class="square">
+									<i></i><i></i>
+									<p>Wulf Kaal is a leading expert at the intersection of law, business, and technology. His research focuses on innovation, technology, blockchain technology applications, smart contracts, initial coin offerings, hedge funds, and dynamic regulatory methods.  Kaal is the director of the Private Investment Fund Institute(PIFI)</p>
+								</div>
+							</div>
+						</div>
+
+						<div class="team_item">
+							<div class="img-circle">
+								<img src="/img/team/dmitry.svg" alt="">
+							</div>
+							<div class="team_txt">
+								<div class="b12-team-txt"><h4>Dmitry Samoylovskikh</h4><h4>Technology</h4></div>
+								<div class="square">
+									<i></i><i></i>
+									<p>Forbes 30 Under 30  Co-founder of Tesla Amazing — successful international company, funded on Kickstarter: top 2 projects during the whole campaign. All obligation are fulfilled on time.<br> Now: distribution network in 37 countries and laboratory focusing on disruptive green energy technologies.<br> Higher school of economics - Master's.</p>
+								</div>
+							</div>
+						</div>
+
+						<div class="team_item">
+							<div class="img-circle">
+								<img src="/img/team/sergey.svg" alt="">
+							</div>
+							<div class="team_txt">
+								<div class="b12-team-txt"><h4>Sergey Tsvetkov</h4><h4>Technology</h4></div>
+								<div class="square">
+									<i></i><i></i>
+									<p>Molecular physics: physics of nuclear reactors. Author and co-author of several patents for cold fusion, both Russian and European.<br> Multiple works published in scientific articles. Has been conducting research and development since 1989.<br>Physics-technics faculty of Ural<br> Polytechnics institute.</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="btn-popup text-center mg-t-5">
+						<a href="#">SEE FULL TEAM</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section id="b13">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-xl-12">
+					<h1 class="standart-h1">ROADMAP</h1>
+					<div class="wrap-b13">
+						<div class="road">
+							<div class="square">
+								<i></i><i></i>
+								<span id="r-map-line"></span>
+								<h2>September 1989</h2>
+								<p>Research and Development started</p>
+							</div>
+							<div class="square">
+								<i></i><i></i>
+								<span class="rmap-left"></span>
+								<h2>July 2012</h2>
+								<p>1st Successful test conducted</p>
+							</div>
+							<div class="square">
+								<i></i><i></i>
+								<span class="rmap-right"></span>
+								<h2>August 2013</h2>
+								<p>European Patent Filed</p>
+							</div>
+							<div class="square">
+								<i></i><i></i>
+								<span class="rmap-left"></span>
+								<h2>March 2017</h2>
+								<p>New lab opened </p>
+							</div>
+							<div class="square">
+								<i></i><i></i>
+								<span class="rmap-right"></span>
+								<h2>May 2018</h2>
+								<p>Private Token Sales completed</p>
+							</div>
+							<div class="square">
+								<i></i><i></i>
+								<span class="rmap-left-cr"></span>
+								<h2>AUGUST 2018</h2>
+								<p>G3 Prototype Constructed</p>
+							</div>
+							<div class="square">
+								<i></i><i></i>
+								<span class="rmap-right-cr"></span>
+								<h2>SEPTEMBER 2018</h2>
+								<p>Online Streaming begins</p>
+							</div>
+							<div class="square">
+								<i></i><i></i>
+								<span class="rmap-left-cr"></span>
+								<h2>OCTOBER 2018</h2>
+								<p>Pre-ICO completed</p>
+							</div>
+							<div class="square max-150">
+								<i></i><i></i>
+								<span class="rmap-right-cr"></span>
+								<h2>NOVEMBER 2018</h2>
+								<p>Retail-ready Power Station construction begins</p>
+							</div>
+							<div class="square">
+								<i></i><i></i>
+								<span class="rmap-left-cr"></span>
+								<h2>JANUARY 2019</h2>
+								<p>ICO completed</p>
+							</div>
+							<div class="square">
+								<i></i><i></i>
+								<span class="rmap-right-cr"></span>
+								<h2>APRIL 2019</h2>
+								<p>All patents application submitted</p>
+							</div>
+							<div class="square max-150">
+								<i></i><i></i>
+								<span class="rmap-left-cr"></span>
+								<h2>JUNE 2019</h2>
+								<p>Retail-ready Power Station construction completed</p>
+							</div>
+							<div class="square">
+								<i></i><i></i>
+								<span class="rmap-right-cr"></span>
+								<h2>OCTOBER 2019</h2>
+								<p>Retail-ready Power Station tests completed</p>
+							</div>
+							<div class="square">
+								<i></i><i></i>
+								<span class="rmap-left-cr"></span>
+								<h2>NOVEMBER 2019</h2>
+								<p>Certification process completed</p>
+							</div>
+							<div class="square">
+								<i></i><i></i>
+								<span class="rmap-right-cr"></span>
+								<h2>DECEMBER 2019</h2>
+								<p>First deliveries to Token Holders</p>
+							</div>
+							<div class="square">
+								<i></i><i></i>
+								<span class="rmap-left-cr"></span>
+								<h2>JUNE 2020</h2>
+								<p>Sales to the grid begin</p>
+							</div>
+							<div class="square">
+								<i></i><i></i>
+								<span class="rmap-right-cr"></span>
+								<h2>DECEMBER 2020</h2>
+								<p>Retail sales begin</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<footer>
+		
+	</footer>
+
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
