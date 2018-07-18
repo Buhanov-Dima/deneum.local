2f9a30d4ecbcf9ad4219813a14ad990e1c080bbc
DmitriOkulov <dmitriokulov@yandex.ru>
2018-07-18 21:26:57 +0500

    fx

--- mailpost.php
+++ mailpost.php
@@ -1,19 +1,43 @@
-<?if($_POST['mail']=='')
+<?php if($_POST['phone']=='')
 {
 	echo 'error-1';
 }
 else
 {
-	$_POST["mail"] =  substr(htmlspecialchars(trim($_POST['mail'])), 0, 100);
-	$mPhone = $_POST["mail"];
+	$_POST["phone"] =  substr(htmlspecialchars(trim($_POST['phone'])), 0, 100);
+	$mPhone = $_POST["phone"];
 
-	$tokens_path = __DIR__.'/adr697p.txt';
-	$tokens_file = file_get_contents($tokens_path);
-	$addresses = explode("\n", $tokens_file);
-	$addresses[]=$mPhone;
-	$addresses_file = implode("\n", $addresses);
-	file_put_contents($tokens_path, $addresses_file);
+	$_POST["reason"] =  substr(htmlspecialchars(trim($_POST['reason'])), 0, 300);
+	$mReason = $_POST["reason"];
 
+	$_POST["name"] =  substr(htmlspecialchars(trim($_POST['name'])), 0, 100);
+	$mName = $_POST["name"];
+
+	$office_id = 3194;
+	$source_id = 10478;
+	 
+	$params = [
+	    'firstname' => $mName,
+	    'phone' => $mPhone,
+	    'office_id' => $office_id,
+	    'source_id' => $source_id,
+];
+
+
+	$curl = curl_init();
+ 
+    curl_setopt($curl, CURLOPT_URL, 'https://api.crmramex.ru/treatment/create?token=2039.dae843355c3ad46692383f46802f2f78480e64dec2602afc5ccd9410a7a11a8c');
+    curl_setopt($curl, CURLOPT_TIMEOUT, 60);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+ 
+    if ($params !== null && is_array($params)) {
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $params);
+    }
+    $result = curl_exec($curl);
+    curl_close($curl);
+ 
+    print_r(json_decode($result, true));
 
 	
 }
